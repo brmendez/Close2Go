e906905292cdeffe3a30f09749fee47f19619410
Brian Mendez <brmendez@gmail.com>
2016-03-14 20:04:35 -0700

    debugging Heroku issue

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use('/getCarData', getCarData);
 //app.listen(port);
 
 // lets the port be set up by Heroku
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
